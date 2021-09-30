73f39a69fee9e5fb4431e6daab1bd038e70f4666
Lalu Muhamad Waisul Kuroni <54065417+waisul@users.noreply.github.com>
2021-09-30 14:08:21 +0800

    Create HistoryActivity.java

---
+++ app/src/main/java/com/example/gotravel/HistoryActivity.java
@@ -0,0 +1,21 @@
+package com.example.gotravel;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nongsa.gotravel.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
